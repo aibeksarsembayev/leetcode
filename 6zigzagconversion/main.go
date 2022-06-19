b75d2150e2ca05a5de9f71a0c32e2586e3da02a4
quazar <s-bek-k@mail.ru>
2022-06-19 20:43:28 +0600

    6zigzag solved

--- 6zigzagconversion/main.go
+++ 6zigzagconversion/main.go
@@ -5,31 +5,45 @@ import "fmt"
 func main() {
 	fmt.Println(convert("PAYPALISHIRING", 3))
 	fmt.Println(convert("PAYPALISHIRING", 4))
+	fmt.Println(convert("A", 1))
 
 }
 
 func convert(s string, numRows int) string {
+	if numRows == 1 {
+		return s
+	}
 	var temp string
-	strArr := make([]string, numRows)
-	divider := numRows + numRows - 1
-	fmt.Println(divider)
-	for i, v := range s {
-		var arrIndex int
-		if i%divider < numRows+1 {
 
-			arrIndex = i % numRows
-			strArr[arrIndex] += string(v)
-			fmt.Println(arrIndex, string(v))
+	strArr := make([]string, numRows)
+
+	joining := numRows - 2
+
+	fmt.Println(joining)
+
+	for i, v := range s {
+
+		var arrIndex int
+
+		if i%(numRows+joining) < numRows {
+
+			arrIndex = i % (numRows+joining)
+
 		} else {
 
-			// arrIndex = numRows - (i+1)%numRows
+			arrIndex = numRows - ((i % (numRows+joining)) - numRows + 2)
+
 		}
-		// strArr[arrIndex] += string(v)
+
+		strArr[arrIndex] += string(v)
 
 	}
+
 	for _, v := range strArr {
 		temp += v
 	}
+
 	fmt.Println(strArr)
+
 	return temp
 }
